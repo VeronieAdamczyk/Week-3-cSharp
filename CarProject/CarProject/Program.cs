190c583e94cf0760df0ed29bfca148683cf68cc0
VeronieAdamczyk <42538456+VeronieAdamczyk@users.noreply.github.com>
2018-09-07 16:05:13 +0100

    Add files via upload

---
+++ CarProject/CarProject/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject
+{
+    // Car class starts
+    class Car
+    {
+        public string make  { get; set; }
+        public string model { get; set; }
+        public int    price  { get; set; }
+        //public static int numberOfCars;
+
+       public Car(string make, string model, int price)
+           {
+
+            this.make= make;
+            this.model = model;
+            this.price = price;
+        }
+
+   public static Car CreateCar()
+        {
+            string make;
+            string model;
+            int price;
+
+            Console.WriteLine("please enter a make:");
+            make = Console.ReadLine();
+            Console.WriteLine("please enter a model:");
+            model = Console.ReadLine();
+            Console.WriteLine("please enter a price:");
+            price = Convert.ToInt32(Console.ReadLine());
+
+            Car myCar = new Car(make, model, price);
+            return myCar;
+        }    
+
+        public static void DisplayAllCars(List<Car> allCars)
+        {
+            Console.WriteLine("Here is a list of the cars currently stored: ");
+
+            foreach (Car car in allCars)
+            {
+                Console.WriteLine("Make: {0}", car.make);
+                Console.WriteLine("Model: {0}", car.model);
+                Console.WriteLine("Price: £{0}", car.price);  
+            }
+        }
+
+
+       
+    } // End of car class
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string response;
+
+            List<Car> CarMake = new List<Car>();
+         
+            do
+            {
+                Console.WriteLine("Please type A to add a car to the list, D to Delete a car from the list, L to view the list of cars or X to Exit:");
+                response = Console.ReadLine().ToUpper();
+
+                if (response == "A")
+                {
+                    CarMake.Add(Car.CreateCar());
+                }
+                if(response=="D")
+                {
+                    CarMake.Remove(Car.CreateCar());
+                }
+                if(response == "L")
+                {
+                    Car.DisplayAllCars(CarMake);
+                }
+            } while (response != "X");
+           
+            Console.WriteLine(CarMake.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
+ 
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IList<Employee> EmployeeList = new List<Employee>()
+            {
+                new Employee() {EmployeeName= "John", EmployeeID= 456, EmployeePhone = "12356745"},
+                new Employee() {EmployeeName= "Mark", EmployeeID= 657, EmployeePhone = "57868902"},
+                new Employee() {EmployeeName= "Mike", EmployeeID= 896, EmployeePhone = "63878673"},
+            };
+
+            Console.WriteLine(EmployeeList.Count());
+            foreach(Employee e in EmployeeList)
+            {
+                Console.WriteLine("employee name is- {0}, Employee ID is- {1}, their phone number is- {2}", e.EmployeeName, e.EmployeeID, e.EmployeePhone);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Employee
+    {
+
+        public string EmployeeName { get; set; }
+        public int EmployeeID { get; set; }
+        public string EmployeePhone { get; set; }
+    }
+
+}
+
+
+
